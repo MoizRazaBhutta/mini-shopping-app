4c86d810a38f227469fd6eb54b0abbc176fab0e6
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-11-03 17:23:05 -0500

    Logout strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from './services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,19 @@ import { CartService } from './services/cart.service';
 })
 export class AppComponent implements OnInit {
   cartItemCount = 0;
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService,private router: Router) {}
   ngOnInit() {
     this.cartService.cartCount$.subscribe((count) => {
       this.cartItemCount = count
     })
   }
+
+  logout() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('email');
+    // localStorage.removeItem('cartItemCount');
+    this.cartService.cartCount.next(0); 
+    this.router.navigate(['/login']); 
+  }
+
 }
