fede08223a0913b5e3d8dda96992e18712281413
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-11-03 17:41:49 -0500

    Cart fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from './services/cart.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  cartItemCount = 0;
+  cartItemCount$:Observable<number>;
   constructor(private cartService: CartService,private router: Router) {}
   ngOnInit() {
-    this.cartService.cartCount$.subscribe((count) => {
-      this.cartItemCount = count
-    })
+    this.cartItemCount$ =this.cartService.cartCount$;
   }
 
   logout() {
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  cartCount:Subject<number>=new Subject<number>();
+  cartCount:BehaviorSubject<number>=new BehaviorSubject<number>(0);
   cartCount$=this.cartCount.asObservable();
   cartItems:any[]=[
     
