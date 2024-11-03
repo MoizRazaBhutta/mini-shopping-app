e1f3182a102d9a68626f8a9fa1b0ac371ec8126b
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-11-03 17:17:05 -0500

    Cart Icon update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'mini-shopping-app';
-  isLoggedIn = false; 
-
+  cartItemCount = 0;
+  constructor(private cartService: CartService) {}
   ngOnInit() {
-    const user = localStorage.getItem('loggedInUser');
-    if (user) {
-      this.isLoggedIn = true;
-    }
+    this.cartService.cartCount$.subscribe((count) => {
+      this.cartItemCount = count
+    })
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  cartCount:Subject<number>=new Subject<number>();
+  cartCount$=this.cartCount.asObservable();
+  cartItems:any[]=[
+    
+  ];
   constructor() { }
+
+  addToCart(){
+    this.cartCount.next(this.cartItems.length);
+  }
 }
