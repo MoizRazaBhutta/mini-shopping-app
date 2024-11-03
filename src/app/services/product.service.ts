b61885f9fcccb73a1e962bf39fe1c5e898644a22
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-11-03 18:43:20 -0500

    Persisting products using local storage

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/products';
 
@@ -10,10 +10,24 @@ import { Product } from '../models/products';
 export class ProductService {
   private baseUrl = environment.BASE_URL;
   constructor(private http: HttpClient) { }
+  private products : Product[] = [];
 
   getAllProducts():Observable<Product[]> {
+    if (this.getStoredProducts()) {
+      this.products = this.getStoredProducts();
+      return of(this.products);
+    }
     return this.http.get<Product[]>(`${this.baseUrl}/products`).pipe(
-      tap(data => console.log(data))
+      tap(data => this.storeProducts(data))
     );
+  }
+
+  private storeProducts(products: Product[]): void {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  private getStoredProducts(): Product[] | null {
+    const products = localStorage.getItem('products');
+    return products ? JSON.parse(products) : null;
   }
 }
